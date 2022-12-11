233a210eef86f06547fcb7c28a5e1eb0cb41533d
isaac <isaactonyloi@gmail.com>
2022-12-11 10:18:10 +0300

    two sum solution

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  // for(let i = 0; i < array.length; i++) {
+  //   for (let j = i + 1; j < array.length; j++) {
+  //     if (array[i] + array[j] == target) {
+  //       return true;
+  //     } else {
+  //       return false;
+  //     }
+
+  //   }
+  //}
+
+  const seenNumbers = {};
+  for (const number of array) {
+    // n steps
+    const complement = target - number;
+    // n steps
+    if (seenNumbers[complement]) return true;
+    // n steps
+    seenNumbers[number] = true;
+  }
+  // 1 step
+  return false;
+
+
 }
+
+hasTargetSum([22, 19, 4, 6, 30], 25);
 
 /* 
   Write the Big O time complexity of your function here
@@ -16,19 +42,19 @@ function hasTargetSum(array, target) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+//   // add your own custom tests in here
+console.log("Expecting: true");
+console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
-  console.log("");
+console.log("");
 
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+console.log("Expecting: true");
+console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
+ console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+console.log("Expecting: false");
+console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
 
 module.exports = hasTargetSum;
